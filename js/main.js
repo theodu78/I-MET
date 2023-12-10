6c9e299be38e8d171dc7c2931c929425057a944f
théo <supertoto29@hotmail.fr>
2023-12-10 16:52:42 +0100

    date JJ/MM

--- js/main.js
+++ js/main.js
@@ -18,27 +18,49 @@ document.addEventListener('DOMContentLoaded', function() {
     calendar.render();
 });
 
-function ouvrirModal(date) {
-    document.getElementById('dateDebut').value = date;
-    document.getElementById('dateFin').value = date;
+function convertirDateEnISO(dateFrancaise) {
+    var parties = dateFrancaise.split('/');
+    var jour = parties[0];
+    var mois = parties[1];
+    var annee = parties[2];
+    return `${annee}-${mois}-${jour}`; // Format ISO (YYYY-MM-DD)
+}
+
+
+function formaterDate(dateString) {
+    var date = new Date(dateString);
+    var jour = ('0' + date.getDate()).slice(-2); // Ajoute un 0 si nécessaire
+    var mois = ('0' + (date.getMonth() + 1)).slice(-2); // Les mois sont de 0 à 11
+    var annee = date.getFullYear();
+    return `${jour}/${mois}/${annee}`; // Format DD/MM/YYYY
+}
+
+function ouvrirModal(dateISO) {
+    var dateFormatee = formaterDate(dateISO);
+    document.getElementById('dateDebut').value = dateFormatee;
+    document.getElementById('dateFin').value = dateFormatee;
     document.getElementById('modalPopup').style.display = 'block';
 }
+
 
 function fermerModal() {
     document.getElementById('modalPopup').style.display = 'none';
 }
 
 function sauvegarderPresence() {
-    var dateDebut = document.getElementById('dateDebut').value;
-    var dateFin = document.getElementById('dateFin').value;
+    var dateDebutFrancaise = document.getElementById('dateDebut').value;
+    var dateFinFrancaise = document.getElementById('dateFin').value;
+
+    var dateDebutISO = convertirDateEnISO(dateDebutFrancaise);
+    var dateFinISO = convertirDateEnISO(dateFinFrancaise);
     
     var nouvelEvenement = {
         title: 'Présence enregistrée',
-        start: dateDebut,
-        end: dateFin
+        start: dateDebutISO,
+        end: dateFinISO
     };
 
-    calendar.addEvent(nouvelEvenement); // Utilisation de la référence globale
+    calendar.addEvent(nouvelEvenement);
 
     var presencesExistantes = recupererPresences() || [];
     presencesExistantes.push(nouvelEvenement);
@@ -46,3 +68,4 @@ function sauvegarderPresence() {
 
     fermerModal();
 }
+
